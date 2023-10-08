71b5876d49a295177e9a2b8deea509887f15454e
EricNorrwing <eric.norrwing@gmail.com>
2023-10-08 15:50:23 +0200

    final push for Lek 1

---
+++ src/com/EricNorrwing/lektion1tasks/Main.java
@@ -0,0 +1,36 @@
+package com.EricNorrwing.lektion1tasks;
+
+import java.io.PrintStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //Part 1
+        int x = 15;
+        int y = 25;
+        System.out.println(x+y);
+
+        //Part 2
+        int[] score = {0,50,100};
+        System.out.println(score[0]);
+
+        //Part 3
+        String benny = "Benny";
+        String anton = "Anton";
+        System.out.println(benny+anton);
+
+        //Part 4, tested
+
+        //Part 5, tested
+
+        //Part 6
+        int number1 = 10;
+        int Number1 = 25;
+        System.out.println(number1);
+        System.out.println(Number1);
+
+
+
+    }
+}
